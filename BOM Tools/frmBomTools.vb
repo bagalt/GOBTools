5bee73864cb8631a237a3af24c298a28876812cf
GOB <brett.gombar@mikron.com>
2018-11-26 06:30:41 -0700

    changed version num from 0.6 to 1.5

--- BOM Tools/frmBomTools.vb
+++ BOM Tools/frmBomTools.vb
@@ -37,7 +37,7 @@ Public Class frmBomTools
             mAssyDoc = g_inventorApplication.ActiveDocument
             'get the top level assembly document name
             startAssy = mAssyDoc.PropertySets.Item("Design Tracking Properties").Item("Part Number").Value
-            lblVersion.Text = "v0.6"
+            lblVersion.Text = "v1.5"
 
             'define colors for row highlighting
             colorPartNotOnList = Color.DeepPink
