0e7fdda60d468267549f3e3c7e6656bb4614c951
GOB <brett.gombar@mikron.com>
2019-02-06 14:38:49 -0700

    bumped version

--- StepperForm.vb
+++ StepperForm.vb
@@ -7,7 +7,7 @@ Imports System.Drawing
 Public Class frmStepper
 
     Dim gAssyCompDef As Inventor.AssemblyComponentDefinition
-    Dim gdblPosArray As Object(,)
+    Dim gdblPosArray As Object(,) 'array from VNM has 361 elements, arrays start at zero
     Dim gintCurrentIndex As Integer = 1 'holds current index for traversing through position array
     Dim gintPrevIndex As Integer = 1 'holds previous index for traversing through position array
     Public gInvApp As Inventor.Application 'global for inventor application object
